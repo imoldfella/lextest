6211ea44c92ad59c9474c9632695a52efc9d38ee
Jim Hurd <jimh@datagrove.com>
2023-07-10 19:39:33 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { PlaygroundEditor } from './editor'
+
+function App() {
+  return (
+    <>
+      <PlaygroundEditor/>
+    </>
+  )
+}
+
+export default App
